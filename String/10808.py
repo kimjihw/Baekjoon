6f4953d16e47cf6a7013a4cd3c4c5dff6494ac4e
kimjihw <kimal846@naver.com>
2021-12-10 20:52:48 +0900

    clear

--- String/10808.py
+++ String/10808.py
@@ -1,8 +1,7 @@
 s = str(input())
 lst = [0] * 26
-rst = ""
 for i in range(len(s)):
     lst[ord(s[i]) - 97] = s.count(s[i])
+
 for i in lst:
-    rst += str(i)
-print(" ".join(rst))
+    print(i, end=" ")
