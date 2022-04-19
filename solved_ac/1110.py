4a9dc089ca1a673e316334480f552df0c7459b46
jihwan <kimal846>
2022-04-19 18:11:45 +0900

    clear

---
+++ solved_ac/1110.py
@@ -0,0 +1,11 @@
+num = num2 = int(input())
+count = 0
+while True:
+    ten = num // 10
+    one = num % 10
+    total = ten + one
+    count += 1
+    num = int(str(num%10) + str(total % 10))
+    if (num == num2):
+        break
+print (count)
