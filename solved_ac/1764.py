050e0e620125f86dca6db1dd629f84c286fec84d
지환 <57333984+kimjihw@users.noreply.github.com>
2023-05-25 23:11:13 +0900

    clear -> 결국 n까지와 n부터와 서로 다른 거 책정하면 됨

--- solved_ac/1764.py
+++ solved_ac/1764.py
@@ -6,9 +6,10 @@ lst = []
 for i in range(n + m):
     lst.append(str(sys.stdin.readline().strip()))
 
-not_see = set(lst[m:])
-not_listen = set(lst[:n])
+not_see = set(lst[:n])
+not_listen = set(lst[n:])
 
+print(not_see, not_listen)
 result = sorted(list(not_see.intersection(not_listen)))
 
 print(len(result), "\n".join(result), sep="\n")
